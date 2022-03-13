88c5dd353e704eaff5d40be0e49fc3d19f0567fd
David <david.nissimoff@gmail.com>
2022-03-12 22:20:17 -0800

    .

--- src/BabyKusto.Core/EvaluationResult.cs
+++ src/BabyKusto.Core/EvaluationResult.cs
@@ -10,7 +10,7 @@ namespace BabyKusto.Core.Evaluation
     {
         protected EvaluationResult(TypeSymbol type)
         {
-            Type = typo;
+            Type = typoo;
         }
 
         public TypeSymbol Type { get; }
